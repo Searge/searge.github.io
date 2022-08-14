506b488508d905c3c50c69920480811f6cdcc69d
Searge <ashikcygan@gmail.com>
2022-08-14 22:26:20 +0300

    Assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,48 @@
 /* eslint-disable no-console */
-const gulp = require("gulp");
-const pug = require("gulp-pug");
-const sass = require("gulp-sass")(require("sass"));
-const htmlmin = require("gulp-htmlmin");
-const cleanCSS = require("gulp-clean-css");
-const inlinesource = require("gulp-inline-source");
-const path = require("path");
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass')(require('sass'));
+const htmlmin = require('gulp-htmlmin');
+const cleanCSS = require('gulp-clean-css');
+const inlinesource = require('gulp-inline-source');
+const path = require('path');
 // const del = require("del");
 
 const html = () =>
   gulp
-    .src("./src/pug/views/*.pug")
+    .src('./src/pug/views/*.pug')
     .pipe(pug({ pretty: true }))
-    .pipe(inlinesource({ rootpath: path.resolve("dist") }))
+    .pipe(inlinesource({ rootpath: path.resolve('dist') }))
     .pipe(
       htmlmin({
         collapseWhitespace: true,
         // removeOptionalTags: true,
         collapseBooleanAttributes: true,
-      })
+      }),
     )
-    .pipe(gulp.dest("dist"));
+    .pipe(gulp.dest('dist'));
 
 const css = () =>
   gulp
-    .src("./src/sass/*.scss")
-    .pipe(sass().on("error", sass.logError))
+    .src('./src/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
     .pipe(
-      cleanCSS({ debug: true, level: 2 }, (details) => {
+      cleanCSS({ debug: true, level: 2 }, details => {
         console.log(`${details.name}: ${details.stats.originalSize}`);
         console.log(`${details.name}: ${details.stats.minifiedSize}`);
-      })
+      }),
     )
-    .pipe(gulp.dest("dist/styles"));
+    .pipe(gulp.dest('dist/styles'));
 
 // const purge = () => del(["dist/styles"]);
 
-const assets = () => gulp.src("public/*").pipe(gulp.dest("dist"));
+const assets = () => gulp.src('public/**/*').pipe(gulp.dest('dist'));
 
 const dev = () =>
   gulp.watch(
-    ["src/**/*"],
+    ['src/**/*'],
     { ignoreInitial: false },
-    gulp.series(css, html, assets)
+    gulp.series(css, html, assets),
   );
 
 exports.html = html;

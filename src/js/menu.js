4ed98ae9f3cb33471d2c5769689a105152e8c2c8
Searge <ashikcygan@gmail.com>
2022-08-31 19:36:17 +0000

    Deploying to gh-pages from @ Searge/searge.github.io@61442bb5c5a095b2e7d83128dd3fc457a998e20f 🚀

--- src/js/menu.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  const menuLink = document.querySelector('.menu__link');
-
-  if (menuLink.getAttribute('href') === window.location.href) {
-    menuLink.classList.toggle('menu__link--current');
-  }
-})();
